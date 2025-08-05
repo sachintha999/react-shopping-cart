5e2c67fcf7245acea7712104223fdffb82859e71
sachintha999 <sachinthadilshan8332@gmail.com>
2025-08-05 15:33:57 +0530

    product grid price and view product section develop

--- src/components/productTile/index.jsx
+++ src/components/productTile/index.jsx
@@ -12,7 +12,11 @@ function ProductTile({ singleProductTile }) {
                 <div className="font-bold text-gray-900 sm:text-sm text-xs md:text-base">
                     <p className="w-[100px] overflow-hidden text-ellipsis white-space-nowrap">{singleProductTile?.title}</p>
                 </div>
+                <div className="text-right ">
+                    <p className="text-xs font-bold text-gray-900 sm:text-sm md:text-[14px]">${singleProductTile?.price}</p>
+                </div>
             </div>
+            <button className="mt-4 text-xs font-semibold text-cyan-700">View Details</button>
         </div>
     );
 }
