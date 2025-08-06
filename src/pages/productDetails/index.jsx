315609768386cb842e3d0f4ce0de4854a17f9ad2
sachintha999 <sachinthadilshan8332@gmail.com>
2025-08-06 11:03:31 +0530

    product inner page develop

--- src/pages/productDetails/index.jsx
+++ src/pages/productDetails/index.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { shoppingCartContext } from "../../context";
 
 
 
@@ -26,7 +27,29 @@ function productDetails() {
    console.log(productDetails);
 
     return (
-        <div>productDetails</div>
+       <div>
+        <div className="p-6 lg:max-w-7xl max-w-4xl mx-auto">
+            <div className="grid items-center grid-cols-1 lg:grid-cols-5 gap-12 shadox-sm p-6" >
+                <div className="lg:col-span-3 w-full lg:sticky top-0 text-center">
+
+                    <div className="px-4 py-10 rounded-xl shadow-lg relative">
+
+                        <img
+                        className="w-4/5 rounded object-cover"
+                        src={productDetails?.thumbnail}
+                        alt={productDetails?.title}
+                        />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+       </div>
     )
 }
 
